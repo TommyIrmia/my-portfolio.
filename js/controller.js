802e4489ef03f262e81373eca3e63468bc030de7
TommyIrmia <tommyirmia@gmail.com>
2021-08-02 21:53:48 +0300

    changed styling

--- js/controller.js
+++ js/controller.js
@@ -13,14 +13,14 @@ function renderGrid() {
     strHTMLs = projs.map(function(proj) {
         var label1 = proj.labels[0]
         var label2 = proj.labels[1]
-        return `<div class="col-md-4 col-sm-6 portfolio-item">
+        return `<div class="col-md-4 col-sm-6 portfolio-item ">
         <a class="portfolio-link" data-toggle="modal" href="#portfolioModal" onclick="renderModal('${proj.id}')">
         <div class="portfolio-hover">
         <div class="portfolio-hover-content">
         <i class="fa fa-plus fa-3x"></i>
         </div>
         </div>
-        <img class="img-fluid" src="img/portfolio/${proj.id}.png" alt="">
+        <img class="img-fluid" src="img/portfolio/${proj.id}.png" alt="${proj.name}">
         </a>
         <div class="portfolio-caption">
         <h4>${label1}</h4>
